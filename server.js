720ea126231af4bd09d9bddd7da66a87e7ab212e
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2016-09-02 17:27:29 +0300

    Refactoring server.js

--- server.js
+++ server.js
@@ -1,41 +1,40 @@
 /* eslint-disable */
 const express = require('express');
-const config = require('./webpack.config')[0];
-const configExt = require('./webpack.config')[1];
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
 
-const webpack = require('webpack');
 const bodyParser = require('body-parser');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
 // var getIpAddr = require('./app/modules/helpers/getIpAddr');
 
 // Require Routes
 const rootRoute = require('./app/routes/rootRoute');
 const registerRoute = require('./app/routes/registerRoute');
-const webpackDevMiddConfig = {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-}
 
 const app = new (require('express'))();
 const port = 3000;
 const host = 'localhost';//getIpAddr();
+
+// ======== WEBPACK ============================================================
+const config = require('./webpack.config')[0];
+const configExt = require('./webpack.config')[1];
+
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+
+const webpackDevMiddConfig = { noInfo:true,publicPath: config.output.publicPath}
 const compiler = webpack(config);
 const compilerExt = webpack(configExt);
 
-compilerExt.watch({
-    aggregateTimeout: 300,
-    poll: true,
-}, function(err, stats) {
-    if(err) {
-      console.log('Extension built with errors:', err);
-    } else {
-      console.log('Extension built', stats.hash);
-    }
-});
+compilerExt.watch(
+  {aggregateTimeout: 300, poll: true,},
+  (err, stats) => console.log(`Extension built ${(err) ? err : stats.hash}`)
+);
+
+app.use(webpackDevMiddleware(compiler, webpackDevMiddConfig));
+app.use(webpackHotMiddleware(compiler));
+// #############################################################################
 
 // Session
 mongoose.connect('mongodb://localhost/test');
@@ -44,8 +43,6 @@ app.use(session({
   store: new MongoStore({mongooseConnection: mongoose.connection})
 }));
 
-app.use(webpackDevMiddleware(compiler, webpackDevMiddConfig));
-app.use(webpackHotMiddleware(compiler));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
